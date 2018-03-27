7baa4cf60880e2c423a7e25b6ad42d17491a8e88
Amanda M <amandaseagull3@gmail.com>
2018-03-27 18:53:14 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-   issue = []
-  array.each { |element| issue << element ** 2 }
-  issue
+   numbers = []
+  array.each { |element| numbers << element ** 2 }
+  numbers
   
 end
