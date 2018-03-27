265b418156c0c4d324fece07dabe2ba6c8e14e48
Amanda M <amandaseagull3@gmail.com>
2018-03-27 18:24:01 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   # your code here
   self.map! {|num| num ** 2}
   
-  brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
+  brothers.each{|numbers| num ** 2}
   end
 end
